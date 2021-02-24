6a2fec79268a879944c1d7a5ac94e7f2b46d146b
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2021-02-24 17:28:45 +0900

    Updated README

--- setup.py
+++ setup.py
@@ -18,22 +18,28 @@ class build_ext(base_build_ext):
                         )
         self.distribution.ext_modules[:] = cythonize(ext)
         super().finalize_options()
-        
-setup(name="sfHMM",
-      version="1.1.0",
-      description="step finding based HMM",
-      author="Hanjin Liu",
-      author_email="liuhanjin-sc@g.ecc.u-tokyo.ac.jp",
-      license="GPLv2",
-      packages=find_packages(),
-      setup_requires=['Cython'],
-      install_requires=[
-            "hmmlearn>=0.2.3",
-            "scikit-learn",
-            "matplotlib",
-      ],
-      cmdclass={"build_ext": build_ext},
-      ext_modules=[Extension("", [])],
-      include_dirs=[numpy.get_include()],
-      python_requires=">=3.6",
-      )
+
+kwargs = dict(name="sfHMM",
+              version="1.1.0",
+              description="step finding based HMM",
+              author="Hanjin Liu",
+              author_email="liuhanjin-sc@g.ecc.u-tokyo.ac.jp",
+              license="GPLv2",
+              packages=find_packages(),
+              setup_requires=['Cython'],
+              install_requires=[
+                    "hmmlearn>=0.2.3",
+                    "scikit-learn",
+                    "matplotlib",
+                    ],
+              python_requires=">=3.6"
+              )
+
+try:
+    setup(cmdclass={"build_ext": build_ext},
+          ext_modules=[Extension("", [])],
+          include_dirs=[numpy.get_include()],
+          **kwargs)
+except:
+    # if C compiler is not installed
+    setup(**kwargs)
